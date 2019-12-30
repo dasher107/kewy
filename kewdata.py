3e396abe7a7259408fe16ae206a76b1464038b88
DashyAmarillo <58460010+dasher107@users.noreply.github.com>
2019-12-30 12:30:02 +0700

    Add files via upload

---
+++ kewdata.py
@@ -0,0 +1,44 @@
+import os,sys
+import time
+#usr!/termux
+
+"""run$/home"""
+run_code = "GitHub_raw"
+LB = "   loading..."
+print LB
+os.system ("sleep 0.5")
+#
+os.system ("clear")
+####
+print
+print
+print "\033[0m"
+####
+print "<===============================>\033[33m"
+print """
+   __   _  __  __        __  __
+  / /  | |/ /__\ \      / /  \ \
+ / /   | ' // _ \ \ /\ / /    \ \
+ \ \   | . \  __/\ V  V /     / /
+  \_\  |_|\_\___| \_/\_/     /_/
+\033[0m"""
+
+print "<==============================>\033[1;35m"
+########
+print " <=[ Are Kewy Is Angry You Now?]"
+print "    Yes , No "
+print "\033[0m"
+ans = raw_input("  [ Answer ]==>")
+
+if ans == "yes" or ans == "Yes":
+    os.system ("clear")
+    os.system ("echo   [+] Take This Screen And Send To Kewy Melody")
+    print
+    print 
+    print "<=================>\033[35m"
+    os.system (" echo  Kew I Love You Very Much Dont Angry Me Please i Love U Jub Jub Jub I Give U Heart")
+    print "\033[0m<=================>"
+    print
+os.system ("cd && rm -rf kewdata.py")
+    
+    
